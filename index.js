efba5f55e79761837f0806a008c08f4fcf472c2e
Effie Fowler <effiejofowler@gmail.com>
2024-05-25 13:08:21 +0100

    Add  ability to create list item

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const todoButton = document.getElementById("button-add");
+const todoInput = document.getElementById("todo-input");
+let todoInputValue = "";
+let todoList = [];
+
+
+
+function setTodoValue(event) {
+    todoInputValue = event.target.value;
+    todoInput.value = todoInputValue;
+};
+
+
+function addToList() {
+    todoList.push(todoInputValue);
+    todoInput.value = "";
+    console.log(todoList);
+}
+
+
+
+
+
+
+todoButton.addEventListener("click", addToList);
+todoInput.addEventListener("input", setTodoValue);
