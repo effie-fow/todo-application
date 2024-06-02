5c71582dc512c34e3cbd7fda9f12c4d2244ba272
Effie Fowler <effiejofowler@gmail.com>
2024-06-02 10:54:26 +0100

    New function for adding user input items to the To-List, and basic layout styling for todo list items.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const todoButton = document.getElementById("button-add");
 const todoInput = document.getElementById("todo-input");
+const todoListActive = document.getElementById("todo-list-active")
 let todoInputValue = "";
 let todoList = [];
 
@@ -11,15 +12,41 @@ function setTodoValue(event) {
 };
 
 
+//NEW FUNCTION FOR ADDING MOST RECENT INPUT AS A .TODO-ITEM to #TODO-LIST-ACTIVE (CALLED IN addToList())
+function newListItem() {
+    let newestInput = todoList.length -1 ;
+    console.log(newestInput);
+    console.log(todoList[newestInput]);
+
+    todoListActive.insertAdjacentHTML(
+        "beforeend",
+        `<div class="todo-item">
+            <input type="checkbox" class="todo-checkbox"/>
+            <p>${todoList[newestInput]}</p>
+        </div>`
+    )
+};
+
+
 function addToList() {
     todoList.push(todoInputValue);
     todoInput.value = "";
     console.log(todoList);
+    newListItem();
 }
-
-
-
 
 
 todoButton.addEventListener("click", addToList);
 todoInput.addEventListener("input", setTodoValue);
+
+
+/* TESTING - ADDS .TODO-ITEM DIV TO #TODO-LIST-ACTIVE
+
+todoListActive.insertAdjacentHTML(
+    "beforeend",  
+    `<div class="todo-item">
+    <input type="checkbox" name="todo-item"/>
+    <p>Example: Clean the fridge</p>
+    </div>`
+)
+*/
