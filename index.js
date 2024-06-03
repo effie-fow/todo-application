916d2d21b2d6be306997cd5631cea842706b90c5
Effie Fowler <effiejofowler@gmail.com>
2024-06-03 16:06:14 +0100

    neatened index.js functions

--- index.js
+++ index.js
@@ -20,15 +20,17 @@ function generateUniqueID() {
 
 //NEW FUNCTION FOR ADDING MOST RECENT INPUT AS A .TODO-ITEM to #TODO-LIST-ACTIVE (CALLED IN addToList())
 function newListItem() {
-    let newestInput = todoList.length -1 ;
-    console.log(newestInput);
-    console.log(todoList[newestInput]);
+    let newestInputIndex = todoList.length -1 ;
+    let newestInputStr = todoList[newestInputIndex];
+    
+    console.log(newestInputIndex);
+    console.log(newestInputStr);
 
     todoListActive.insertAdjacentHTML(
         "beforeend",
         `<div class="todo-item" id="todo-item">
             <input type="checkbox" class="todo-checkbox" id="${generateUniqueID()}"/>
-            <p>${todoList[newestInput]}</p>
+            <p>${newestInputStr}</p>
         </div>`
     );
 
